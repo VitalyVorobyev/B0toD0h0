f7bf7f0670f39ed4f2679314f1f63fc7c20e1b2d
VitalyVorobyev <vit.vorobiev@gmail.com>
2015-11-13 18:57:29 +0600

    Signal substitution added

--- MakeNTuple/mvareader.cpp
+++ MakeNTuple/mvareader.cpp
@@ -1,6 +1,16 @@
 #include "mvareader.h"
 
-MVAReader::MVAReader()
+#include "../rooksfw/ksfw_pi0.h"
+#include "../rooksfw/ksfw_etagg.h"
+#include "../rooksfw/ksfw_etappp.h"
+#include "../rooksfw/ksfw_omega.h"
+#include "../rooksfw/ksfw_etapgg.h"
+#include "../rooksfw/ksfw_etapppp.h"
+#include "../rooksfw/ksfw_dst0pi0.h"
+#include "../rooksfw/ksfw_dst0etagg.h"
+#include "../rooksfw/ksfw_dst0etappp.h"
+
+MVAReader::MVAReader(const int mode)
 {
 
 }
--- MakeNTuple/mvareader.h
+++ MakeNTuple/mvareader.h
@@ -1,15 +1,41 @@
 #ifndef MVAREADER_H
 #define MVAREADER_H
 
+#include "icpvevent.h"
 
 class MVAReader
 {
 public:
-    MVAReader();
+    MVAReader(const int mode);
+    double BDT(const TMVAEvent& evt);
+    double lh(const ICPVEvent& evt);
 
-signals:
+private:
+  rooksfw* ksfw1_pi0;
+  rooksfw* ksfw1_etagg;
+  rooksfw* ksfw1_etappp;
+  rooksfw* ksfw1_omega;
+  rooksfw* ksfw0_pi0;
+  rooksfw* ksfw0_etagg;
+  rooksfw* ksfw0_etappp;
+  rooksfw* ksfw0_omega;
 
-public slots:
+  rooksfw* ksfw0_dst0pi0;
+  rooksfw* ksfw0_dst0etagg;
+  rooksfw* ksfw0_dst0etappp;
+  rooksfw* ksfw0_etapgg;
+  rooksfw* ksfw0_etapppp;
+
+  rooksfw* ksfw1_dst0pi0;
+  rooksfw* ksfw1_dst0etagg;
+  rooksfw* ksfw1_dst0etappp;
+  rooksfw* ksfw1_etapgg;
+  rooksfw* ksfw1_etapppp;
+
+  TMVA::Reader* reader_pi0;
+  TMVA::Reader* reader_gg;
+  TMVA::Reader* reader_ppp;
+  TMVA::Reader* reader_omega;
 };
 
 #endif // MVAREADER_H
